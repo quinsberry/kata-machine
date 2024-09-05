40297471d14693bc2066db8dc82c2e83e2f01f8d
ekolesnyk <eugene.kolesnyk@customertimes.com>
2024-09-05 21:04:04 +0200

    feat: improved graphs and Dijkstra algorythm tests

--- src/__tests__/DijkstraList.test.ts
+++ src/__tests__/DijkstraList.test.ts
@@ -1,10 +1,17 @@
 import dijkstra_list from "@code/DijkstraList";
-import { list1 } from "./data/graph";
-import { expect, test } from "bun:test";
+import { circleList1, circleList2, noPathList } from "./data/graph";
+import { describe, expect, test } from "bun:test";
 
-test("dijkstra via adj list", function () {
-    /// waht?
-    // what..
-    // what...
-    expect(dijkstra_list(0, 6, list1)).toEqual([0, 1, 4, 5, 6]);
+describe("dijkstra via adj list", () => {
+    test("dijkstra_list graph with cycle", () => {
+        expect(dijkstra_list(0, 6, circleList1)).toEqual([0, 1, 4, 5, 6]);
+    });
+
+    test("dijkstra_list another graph with cycle", () => {
+        expect(dijkstra_list(0, 3, circleList2)).toEqual([0, 2, 3]);
+    });
+
+    test("dijkstra_list no path", () => {
+        expect(dijkstra_list(0, 3, noPathList)).toEqual([]);
+    });
 });
--- src/__tests__/data/graph.ts
+++ src/__tests__/data/graph.ts
@@ -1,68 +1,97 @@
-export const list1: WeightedAdjacencyList = [];
-
-//      (1) --- (4) ---- (5)
-//    /  |       |       /|
-// (0)   | ------|------- |
-//    \  |/      |        |
-//      (2) --- (3) ---- (6)
-list1[0] = [
+/**
+ * Structure:
+ * 
+ *      (1) ---> (4) ---> (5)
+ *     /  |       |       /|
+ *   (0)  | ------|------- |
+ *     \  |/      |        |
+ *      (2) ---> (3) ---> (6)
+ */
+export const circleList1: WeightedAdjacencyList = [];
+circleList1[0] = [
     { to: 1, weight: 3 },
     { to: 2, weight: 1 },
 ];
-list1[1] = [
+circleList1[1] = [
     { to: 0, weight: 3 },
     { to: 2, weight: 4 },
     { to: 4, weight: 1 },
 ];
-list1[2] = [
+circleList1[2] = [
     { to: 1, weight: 4 },
     { to: 3, weight: 7 },
     { to: 0, weight: 1 },
 ];
-list1[3] = [
+circleList1[3] = [
     { to: 2, weight: 7 },
     { to: 4, weight: 5 },
     { to: 6, weight: 1 },
 ];
-list1[4] = [
+circleList1[4] = [
     { to: 1, weight: 1 },
     { to: 3, weight: 5 },
     { to: 5, weight: 2 },
 ];
-list1[5] = [
+circleList1[5] = [
     { to: 6, weight: 1 },
     { to: 4, weight: 2 },
     { to: 2, weight: 18 },
 ];
-list1[6] = [
+circleList1[6] = [
     { to: 3, weight: 1 },
     { to: 5, weight: 1 },
 ];
 
-export const list2: WeightedAdjacencyList = [];
-
-//     >(1)<--->(4) ---->(5)
-//    /          |       /|
-// (0)     ------|------- |
-//    \   v      v        v
-//     >(2) --> (3) <----(6)
-list2[0] = [
+/**
+ * Structure:
+ * 
+ *      > (1) <--> (4) ----> (5)
+ *     /          |        / |
+ *   (0)    ------|--------  |
+ *     \   v      v         v
+ *      > (2) --> (3) <---- (6)
+ */
+export const circleList2: WeightedAdjacencyList = [];
+circleList2[0] = [
     { to: 1, weight: 3 },
     { to: 2, weight: 1 },
 ];
-list2[1] = [{ to: 4, weight: 1 }];
-list2[2] = [{ to: 3, weight: 7 }];
-list2[3] = [];
-list2[4] = [
+circleList2[1] = [{ to: 4, weight: 1 }];
+circleList2[2] = [{ to: 3, weight: 7 }];
+circleList2[3] = [];
+circleList2[4] = [
     { to: 1, weight: 1 },
     { to: 3, weight: 5 },
     { to: 5, weight: 2 },
 ];
-list2[5] = [
+circleList2[5] = [
     { to: 2, weight: 18 },
     { to: 6, weight: 1 },
 ];
-list2[6] = [{ to: 3, weight: 1 }];
+circleList2[6] = [{ to: 3, weight: 1 }];
+
+/**
+ * Structure:
+ * 
+ *      (0) --> (2)     (1) --> (3)
+ *               |
+ *               v
+ *              (4)     (5)
+ */
+export const noPathList: WeightedAdjacencyList = [];
+
+noPathList[0] = [
+    { to: 2, weight: 1 },
+];
+noPathList[1] = [
+    { to: 3, weight: 1 },
+];
+noPathList[2] = [
+    { to: 4, weight: 1 },
+];
+noPathList[3] = [];
+noPathList[4] = [];
+noPathList[5] = [];
 
 //     >(1)<--->(4) ---->(5)
 //    /          |       /|
