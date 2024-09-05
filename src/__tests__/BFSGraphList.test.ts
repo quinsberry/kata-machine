ad384a4e9be46c3e1692d092fb6276cec9183d44
ekolesnyk <eugene.kolesnyk@customertimes.com>
2024-09-05 15:24:40 +0200

    feat: improved graph tests

--- src/__tests__/BFSGraphList.test.ts
+++ src/__tests__/BFSGraphList.test.ts
@@ -1,9 +1,32 @@
 import bfs from "@code/BFSGraphList";
 import { list2 } from "./data/graph";
-import { expect, test } from "bun:test";
+import { describe, expect, test } from "bun:test";
 
-test("bfs - graph", function () {
-    expect(bfs(list2, 0, 6)).toEqual([0, 1, 4, 5, 6]);
+describe("BFSGraphList", function () {
+    test("graph with no path", function () {
+        const graph = [
+            [{ to: 1, weight: 1 }],
+            [{ to: 2, weight: 1 }],
+            [{ to: 3, weight: 1 }],
+            [],
+        ];
 
-    expect(bfs(list2, 6, 0)).toEqual(null);
+        expect(bfs(graph, 0, 3)).toEqual([0, 1, 2, 3]);
+        expect(bfs(graph, 0, 4)).toEqual(null);
+    });
+
+    test("graph with multiple paths", function () {
+        expect(bfs(list2, 0, 6)).toEqual([0, 1, 4, 5, 6]);
+        expect(bfs(list2, 6, 0)).toEqual(null);
+    });
+
+    test("graph with cycles", function () {
+        const graph = [
+            [{ to: 1, weight: 1 }],
+            [{ to: 2, weight: 1 }],
+            [{ to: 0, weight: 1 }],
+            [{ to: 2, weight: 1 }],
+        ];
+        expect(bfs(graph, 0, 2)).toEqual([0, 1, 2]);
+    });
 });
--- src/__tests__/BFSGraphMatrix.test.ts
+++ src/__tests__/BFSGraphMatrix.test.ts
@@ -1,9 +1,51 @@
 import bfs from "@code/BFSGraphMatrix";
 import { matrix2 } from "./data/graph";
-import { expect, test } from "bun:test";
+import { describe, expect, test } from "bun:test";
 
-test("bfs - graph matrix", function () {
-    expect(bfs(matrix2, 0, 6)).toEqual([0, 1, 4, 5, 6]);
+describe("BFSGraphMatrix", function () {
+    const graph2: WeightedAdjacencyMatrix = [
+        [0, 1, 0, 0],
+        [1, 0, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 1, 0],
+    ];
 
-    expect(bfs(matrix2, 6, 0)).toEqual(null);
+    const graph3: WeightedAdjacencyMatrix = [[0]];
+
+    const graph4: WeightedAdjacencyMatrix = [
+        [0, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [0, 1, 1, 0],
+    ];
+
+    const graph5: WeightedAdjacencyMatrix = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ];
+
+    test("matrix2 path exists", function () {
+        expect(bfs(matrix2, 0, 6)).toEqual([0, 1, 4, 5, 6]);
+    });
+
+    test("matrix2 no path", function () {
+        expect(bfs(matrix2, 6, 0)).toEqual(null);
+    });
+
+    test("graph2 no path", function () {
+        expect(bfs(graph2, 0, 3)).toEqual(null);
+    });
+
+    test("graph3 single node", function () {
+        expect(bfs(graph3, 0, 0)).toEqual([0]);
+    });
+
+    test("graph4 multiple paths", function () {
+        expect(bfs(graph4, 0, 3)).toEqual([0, 1, 3]);
+    });
+
+    test("graph5 disconnected graph", function () {
+        expect(bfs(graph5, 0, 2)).toEqual(null);
+    });
 });
