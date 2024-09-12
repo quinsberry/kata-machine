09cfd8c27c1c0d1df30c7df4ca948b3f3d6dfb02
ekolesnyk <eugene.kolesnyk@customertimes.com>
2024-09-12 15:53:29 +0200

    fix: fixed tests for BFS and DFS on graph list

--- src/__tests__/BFSGraphList.test.ts
+++ src/__tests__/BFSGraphList.test.ts
@@ -1,32 +1,23 @@
 import bfs from "@code/BFSGraphList";
-import { list2 } from "./data/graph";
+import { circleList2 } from "./data/graph";
 import { describe, expect, test } from "bun:test";
 
 describe("BFSGraphList", function () {
-    test("graph with no path", function () {
-        const graph = [
-            [{ to: 1, weight: 1 }],
-            [{ to: 2, weight: 1 }],
-            [{ to: 3, weight: 1 }],
-            [],
-        ];
-
-        expect(bfs(graph, 0, 3)).toEqual([0, 1, 2, 3]);
-        expect(bfs(graph, 0, 4)).toEqual(null);
+    test("finds path in a simple graph", () => {
+        expect(bfs(circleList2, 0, 6)).toEqual([0, 1, 4, 5, 6]);
     });
 
-    test("graph with multiple paths", function () {
-        expect(bfs(list2, 0, 6)).toEqual([0, 1, 4, 5, 6]);
-        expect(bfs(list2, 6, 0)).toEqual(null);
+    test("returns null if no path exists", () => {
+        expect(bfs(circleList2, 6, 0)).toEqual(null);
     });
 
-    test("graph with cycles", function () {
-        const graph = [
-            [{ to: 1, weight: 1 }],
-            [{ to: 2, weight: 1 }],
-            [{ to: 0, weight: 1 }],
-            [{ to: 2, weight: 1 }],
-        ];
-        expect(bfs(graph, 0, 2)).toEqual([0, 1, 2]);
+    test("returns single node path if source is the same as needle", () => {
+        expect(bfs(circleList2, 3, 3)).toEqual([3]);
+    });
+
+    test("handles graph with no edges", () => {
+        const noEdgesGraph = [[]];
+        expect(bfs(noEdgesGraph, 0, 0)).toEqual([0]);
+        expect(bfs(noEdgesGraph, 0, 1)).toEqual(null);
     });
 });
--- src/__tests__/DFSGraphList.test.ts
+++ src/__tests__/DFSGraphList.test.ts
@@ -1,9 +1,23 @@
 import dfs from "@code/DFSGraphList";
-import { list2 } from "./data/graph";
-import { expect, test } from "bun:test";
+import { circleList2 } from "./data/graph";
+import { describe, expect, test } from "bun:test";
 
-test("dfs - graph", function () {
-    expect(dfs(list2, 0, 6)).toEqual([0, 1, 4, 5, 6]);
+describe("DFSGraphList", () => {
+    test("finds path in a simple graph", () => {
+        expect(dfs(circleList2, 0, 6)).toEqual([0, 1, 4, 5, 6]);
+    });
 
-    expect(dfs(list2, 6, 0)).toEqual(null);
+    test("returns null if no path exists", () => {
+        expect(dfs(circleList2, 6, 0)).toEqual(null);
+    });
+
+    test("returns single node path if source is the same as needle", () => {
+        expect(dfs(circleList2, 3, 3)).toEqual([3]);
+    });
+
+    test("handles graph with no edges", () => {
+        const noEdgesGraph = [[]];
+        expect(dfs(noEdgesGraph, 0, 0)).toEqual([0]);
+        expect(dfs(noEdgesGraph, 0, 1)).toEqual(null);
+    });
 });
